4c5985f6c3632795a7646495710410d6a1befc5d
ThiagoSantos7 <seu-email>
2025-04-30 20:48:03 -0300

    projeto water

---
+++ scripts.js
@@ -0,0 +1,19 @@
+const converter = document.querySelector("button");
+const quilos = document.getElementById("input");
+const mensagem = document.querySelector("p");
+
+function conversao() {
+  mensagem.style.display = "block";
+  const input = Number(quilos.value);
+  const result = input * 0.035;
+  const formatResult = result.toFixed(2);
+
+  if (input === 0 || input === null) {
+    mensagem.innerHTML = "Ops, Algo deu errado...";
+  } else {
+    mensagem.innerHTML =
+      "Você deve tomar " + formatResult + "L, de água por dia";
+  }
+}
+
+converter.addEventListener("click", conversao);
